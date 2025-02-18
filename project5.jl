b25ee74b0e87bc91b0efe1455f395580dee31f36
Hugh Shields <hughshields129@gmail.com>
2025-02-18 16:19:01 -0500

    add CO2 data and complete 2

--- project5.jl
+++ project5.jl
@@ -1,7 +1,7 @@
-## --- 1.1
+## --- 1
 using FFTW, Plots, StatGeochem
 
-data = importdataset("BarHarborWL2013.csv", ',', importas=:Tuple)
+data = importdataset("Data/BarHarborWL2013.csv", ',', importas=:Tuple)
 
 # Signal: water Level
 wl = data.Water_Level
@@ -25,4 +25,29 @@ display(time_domain)
 freq_domain = plot(freqs, abs.(F), title="Spectrum", yscale=:log10, framestyle=:box, xlim=(0, 0.5), label="Data")
 vline!([1/24], linestyle=:dash, color=:red, label="1 per Day")
 vline!([1/12], linestyle=:dash, color=:red, label="1 per 12 Hours")
-# display(freq_domain)
+display(freq_domain)
+
+## --- 2
+# Keeling Curve data from http://doi.org/10.6075/J08W3BHW at https://scrippsco2.ucsd.edu/data/atmospheric_co2/primary_mlo_co2_record.html
+data = importdataset("Data/monthly_in_situ_co2_mlo.csv", ',', importas=:Tuple)
+co2 = data.CO2
+N = length(data.CO2)
+# Sample period
+Ts = 1
+# Start and end time
+t0 = 0
+tmax = t0 + N * Ts
+# Time coordinate
+t = t0:Ts:tmax
+
+# Fast Fourier Transform
+ft = fft(co2)
+F = fftshift(ft)
+freqs = fftshift(fftfreq(N, 1.0))
+# Plot results
+time_domain = plot(t[1:end-1]/12, co2, title="Signal", framestyle=:box)
+display(time_domain)
+freq_domain = plot(freqs, abs.(F), title="CO2 Spectrum", yscale=:log10, xlim=(0, 0.5), framestyle=:box, label="Data")
+vline!([1/6], linestyle=:dash, color=:red, label="1 per 6 Months")
+vline!([1/12], linestyle=:dash, color=:red, label="1 per Year")
+display(freq_domain)
